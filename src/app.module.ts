6579b671dd266ca4a3a1988c184c898e8816a23c
Ethan <ethan.collignon.1102@gmail.com>
2026-01-02 03:02:08 +0100

    feat: wip postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        isGlobal: true,
+      }
+    ),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [LogsEntity],
+      synchronize: Boolean(process.env.POSTGRES_SYNCHRONISE)
+    }),
+    TypeOrmModule.forFeature([LogsEntity])
+  ],  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
