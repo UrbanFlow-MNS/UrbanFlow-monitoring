a36503412fdd8fd0c85f0b0a1c77eb37ce42a916
Ethan <ethan.collignon.1102@gmail.com>
2026-01-26 17:38:06 +0100

    feat: datastamp

---
+++ src/Controllers/serverDatastamp.controller.ts
@@ -0,0 +1,36 @@
+import {
+  ServerDatastampBody,
+} from '@bato-urbanflow/urbanflow-models';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ServerDatastampService } from '../Services/serverDatastamp.service';
+
+@Controller('dataLogs')
+export class DataLogsController {
+  constructor(private readonly serverDatastampService: ServerDatastampService) {
+    this.serverDatastampService = serverDatastampService;
+  }
+
+  @Get()
+  async findWithFilters(
+    @Query('numberOfElement') numberOfElement?: number,
+    @Query('startingElement') startingElement?: number,
+    @Query('serverName') serverName?: string,
+    @Query('startDate') startDate?: string,
+    @Query('endDate') endDate?: string,
+  ): Promise<ServerDatastampBody[]> {
+    return await this.serverDatastampService.findWithFilters(
+      numberOfElement,
+      startingElement,
+      serverName,
+      startDate,
+      endDate,
+    );
+  }
+
+  @Post()
+  async addLog(
+    @Body() data: ServerDatastampBody,
+  ): Promise<ServerDatastampBody> {
+    return await this.serverDatastampService.addDatastamp(data);
+  }
+}
---
+++ src/Services/serverDatastamp.service.ts
@@ -0,0 +1,70 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import {
+  DataLogsBody,
+  ServerDatastampBody,
+} from '@bato-urbanflow/urbanflow-models';
+import { DataLogsEntity } from 'src/Objects/Entities/dataLogs.entity';
+import { dateUtils } from '@bato-urbanflow/urbanflow-models';
+import { Like, Repository } from 'typeorm';
+import { IServerDatastampService } from '../Objects/Interfaces/IServerDatastampService';
+import { ServerDatastampEntity } from '../Objects/Entities/serverDatastamp.entity';
+
+@Injectable()
+export class ServerDatastampService implements IServerDatastampService {
+  @InjectRepository(ServerDatastampEntity)
+  public serverDatastampRepository: Repository<ServerDatastampEntity>;
+
+  constructor(
+    @InjectRepository(ServerDatastampEntity)
+    serverDatastampRepository: Repository<ServerDatastampEntity>,
+  ) {
+    this.serverDatastampRepository = serverDatastampRepository;
+  }
+
+  async findWithFilters(
+    numberOfElement?: number,
+    startingElement?: number,
+    serverName?: string,
+    startDate?: string,
+    endDate?: string,
+  ): Promise<ServerDatastampBody[]> {
+    let parsedStartDate: Date | undefined;
+    let parsedEndDate: Date | undefined;
+    if (startDate !== undefined) {
+      parsedStartDate = new Date(Date.parse(startDate));
+    }
+    if (endDate !== undefined) {
+      parsedEndDate = new Date(Date.parse(endDate));
+    }
+
+    const fetchedDatastamp: ServerDatastampBody[] =
+      await this.serverDatastampRepository.find({
+        where: {
+          serverName: Like(`%${serverName}%`),
+          timestamp: dateUtils.getDateFindOperator(
+            parsedStartDate,
+            parsedEndDate,
+          ),
+        },
+      });
+
+    if (startingElement === undefined) {
+      startingElement = 0;
+    } else if (startingElement >= fetchedDatastamp.length) {
+      throw new Error(
+        'The starting element is greater than the number of element',
+      );
+    }
+
+    if (numberOfElement === undefined) {
+      numberOfElement = 50;
+    }
+
+    return fetchedDatastamp.slice(startingElement, numberOfElement);
+  }
+
+  async addDatastamp(data: ServerDatastampBody): Promise<ServerDatastampBody> {
+    return await this.serverDatastampRepository.save(data);
+  }
+}
