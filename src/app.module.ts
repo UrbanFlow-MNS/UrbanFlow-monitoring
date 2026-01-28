77243afe1f3c3b941f7f3d1be22034cbf6906989
Ethan <ethan.collignon.1102@gmail.com>
2026-01-28 14:04:42 +0100

    fix: added missing providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,14 +11,17 @@ import { DataLogsController } from './Controllers/dataLogs.controller';
 import { ExternalApiController } from './Controllers/externalApi.controller';
 import { MicroserviceController } from './Controllers/microservice.controller';
 import { ServerDatastampController } from './Controllers/serverDatastamp.controller';
+import { PrometheusService } from './Services/prometheus.service';
+import { DataLogsService } from './Services/dataLogs.service';
+import { ExternalApiService } from './Services/externalApi.service';
+import { ServerDatastampService } from './Services/serverDatastamp.service';
+import { MicroserviceService } from './Services/microservice.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(
-      {
-        isGlobal: true,
-      }
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
@@ -26,11 +29,54 @@ import { ServerDatastampController } from './Controllers/serverDatastamp.control
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [DataLogsEntity, ExternalApiEntity, MicroserviceEntity, ServerDatastampEntity],
-      synchronize: Boolean(process.env.POSTGRES_SYNCHRONISE)
+      entities: [
+        DataLogsEntity,
+        ExternalApiEntity,
+        MicroserviceEntity,
+        ServerDatastampEntity,
+      ],
+      synchronize: Boolean(process.env.POSTGRES_SYNCHRONISE),
     }),
-    TypeOrmModule.forFeature([DataLogsEntity, ExternalApiEntity, MicroserviceEntity, ServerDatastampEntity])
-  ],  controllers: [AppController, DataLogsController, ExternalApiController, ServerDatastampController, MicroserviceController],
-  providers: [AppService],
+    TypeOrmModule.forFeature([
+      DataLogsEntity,
+      ExternalApiEntity,
+      MicroserviceEntity,
+      ServerDatastampEntity,
+    ]),
+  ],
+  controllers: [
+    DataLogsController,
+    ExternalApiController,
+    ServerDatastampController,
+    MicroserviceController,
+    PrometheusService,
+  ],
+  providers: [
+    DataLogsService,
+    {
+      provide: 'IDataLogsService',
+      useClass: DataLogsService,
+    },
+    ExternalApiService,
+    {
+      provide: 'IExternalApiService',
+      useClass: ExternalApiService,
+    },
+    ServerDatastampService,
+    {
+      provide: 'IServerDatastampService',
+      useClass: ServerDatastampService,
+    },
+    MicroserviceService,
+    {
+      provide: 'IMicroserviceService',
+      useClass: MicroserviceService,
+    },
+    PrometheusService,
+    {
+      provide: 'IPrometheusService',
+      useClass: PrometheusService,
+    },
+  ],
 })
 export class AppModule {}
