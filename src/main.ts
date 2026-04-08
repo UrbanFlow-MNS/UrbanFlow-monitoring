d84354856292f455408c45dc5093cd22026d94ca
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-04-08 08:40:25 +0200

    fix: tcp

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.API_PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Monitoring BATO')
+    .setDescription('API de moniroting du système BATO')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: Number(process.env.TCP_PORT) || 4005
+    },
+  });
+
+  await app.startAllMicroservices()
+  await app.listen(process.env.TCP_PORT ?? 4005);
 }
+
 bootstrap();
